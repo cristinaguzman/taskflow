1e139866a791d1a0236c4f860f52698f59a9d1a5
Criss <cristinaguzman@live.cl>
2025-01-27 13:43:29 -0300

    Eliminar archivo innecesario

--- send_email.php
+++ send_email.php
@@ -1,14 +1,16 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = htmlspecialchars($_POST['name']);
-    $email = htmlspecialchars($_POST['email']);
-    $message = htmlspecialchars($_POST['message']);
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    $name = htmlspecialchars($_GET['name']);
+    $email = htmlspecialchars($_GET['email']);
+    $message = htmlspecialchars($_GET['message']);
 
+    // Configura los detalles del correo
     $to = "cristina@cuatrorlabs.cl"; // Reemplaza con tu dirección de correo
     $subject = "Nuevo mensaje de contacto";
     $body = "Nombre: $name\nCorreo: $email\n\nMensaje:\n$message";
     $headers = "From: $email";
 
+    // Enviar el correo
     if (mail($to, $subject, $body, $headers)) {
         echo "Mensaje enviado con éxito.";
     } else {
